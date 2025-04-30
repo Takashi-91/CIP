0bfbe9a0d73bc236d2b44b0af983c70f5d0ae200
Sage <101599297+SageGod77@users.noreply.github.com>
2025-04-30 17:50:06 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,22 @@ import './App.css'
 import Payments from './components/Payments.jsx'
 import AuthForm from './components/AuthForm.jsx'
 import Home from './pages/Home.jsx'
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import User from "./pages/User";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/authform" element={<AuthForm/>} />
         <Route path="/payments" element={<Payments />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/user" element={<User />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   )
